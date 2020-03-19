cd50466d4056999534d1e952580f4db309008100
yearnZero <azhensmile@gmail.com>
2020-03-19 12:25:20 +0800

    Config local mock

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,13 +1,18 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const webpack = require('webpack');
+const apiMocker = require('mocker-api');
 const baseConfig = require('./webpack.base');
+const projectRoot = process.cwd();
 
 const devConfig = {
   mode: 'development',
   plugins: [new webpack.HotModuleReplacementPlugin()],
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    contentBase: path.join(projectRoot, 'dist'),
+    before(app) {
+      apiMocker(app, path.join(projectRoot, 'mock/index.js'));
+    },
     hot: true,
     stats: 'errors-only'
   },
